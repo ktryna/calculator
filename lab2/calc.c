fd6983d9e8ec4e6f0ed26970d23fc04c4f012955
Kateryna <demianenko.kateruna@lll.kpi.ua>
2024-10-11 11:50:59 +0300

    Add files for lab2

---
+++ lab2/calc.c
@@ -0,0 +1,23 @@
+#include "calc.h"
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        // Обробка ділення на нуль
+        return 0; // Можна додати обробку помилки
+    }
+}
+
